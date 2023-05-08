966e24c4a07a48c29536a3a88c8952df8bda21d6
Raymond Florendo <raymondflorendo.320@gmail.com>
2023-05-08 16:49:30 +0800

    Fix space

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ resource "aws_instance" "web" {
 }
 
 resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
+  name   = "${random_pet.sg.id}-sg"
   vpc_id = data.aws_vpc.selected
   ingress {
     from_port   = 8080
