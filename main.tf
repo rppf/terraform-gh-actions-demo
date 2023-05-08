6bfa693089c9d9190893e921420b065077312519
Raymond Florendo <raymondflorendo.320@gmail.com>
2023-05-08 16:01:55 +0800

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "devhaustech"
 
     workspaces {
       name = "learn-terraform-github-actions"
